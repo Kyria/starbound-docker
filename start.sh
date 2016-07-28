d45492730804f6ccf34fdd5100eb0e424d0a86ed
Guillaume <anakhon@gmail.com>
2016-07-28 19:53:04 +0200

    fix typo

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ groupmod --gid $GID starbound
 chown -R starbound:starbound /opt/starbound/ /start_starbound
 chmod -R 770 /opt/starbound/ /opt/steamcmd/* /start_starbound
 
-exec sudo -E -u starbound /starbound
+exec sudo -E -u starbound /start_starbound
