3b49f12a81da9755ab3a34efa7065867e45b074c
Guillaume <anakhon@gmail.com>
2016-07-28 21:08:38 +0200

    fix some stuff

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ usermod --uid $UID starbound
 groupmod --gid $GID starbound
 
 chown -R starbound:starbound /opt/starbound/ /start_starbound
-chmod -R 770 /opt/starbound/ /opt/steamcmd/* /start_starbound
+chmod -R 775 /opt/starbound/ /opt/steamcmd/* /start_starbound
 
 exec sudo -E -u starbound /start_starbound
