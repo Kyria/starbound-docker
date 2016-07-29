bda23890ce72ca09de186ac5b90f62f0f6767787
root <root@vps295232.ovh.net>
2016-07-29 10:49:11 +0200

    Force HOME to /opt/steamcmd as sudo don't reset it...

--- start.sh
+++ start.sh
@@ -7,4 +7,7 @@ groupmod --gid $GID starbound
 chown -R starbound:starbound /opt/starbound/ /start_starbound
 chmod -R 775 /opt/starbound/ /opt/steamcmd/* /start_starbound
 
+# force to change HOME as sudo don't reset it here...
+export HOME=/opt/steamcmd
+
 exec sudo -E -u starbound /start_starbound
